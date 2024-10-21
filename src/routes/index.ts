184451e4594d5ca0251c0bb8f2a0c760add0048b
Lucas P Souza <107370459+LucasP-Souza@users.noreply.github.com>
2024-10-21 16:07:06 -0300

    Updating documentation

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -5,13 +5,15 @@ import adminRoutes from './admin'
 import authRoutes from './auth'
 import { authAdmin, authNutritionist, authPatient } from '../middlewares/auth/auth.middleware'
 import healthRoutes from './health'
+import swaggerUi from 'swagger-ui-express'
+import swaggerDocs from './../swagger.json'
 
 const routes = Router()
 routes.use('/auth', authRoutes)
 routes.use('/patient', authPatient, patientRoutes)
 routes.use('/nutritionist', authNutritionist, nutritionistRoutes)
 routes.use('/admin', authAdmin, adminRoutes)
-//routes.use('/docs', swagger)
+routes.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerDocs))
 routes.use('/health', healthRoutes)
 
 export default routes
