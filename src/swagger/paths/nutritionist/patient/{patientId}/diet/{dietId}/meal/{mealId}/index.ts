29c421066e8c56d76061a566f2a1542ac2269a9f
Lucas P Souza <107370459+LucasP-Souza@users.noreply.github.com>
2024-11-09 15:35:15 -0300

    Documentation almost finished, error responses need to be improved

--- src/swagger/paths/nutritionist/patient/{patientId}/diet/{dietId}/meal/{mealId}/index.ts
+++ src/swagger/paths/nutritionist/patient/{patientId}/diet/{dietId}/meal/{mealId}/index.ts
@@ -1,4 +1,4 @@
-import { NewMeal_SchemaSwagger } from '../../../../../../../../schemas_and_examples/meal'
+import { GetMeal_SchemaSwagger, NewMeal_SchemaSwagger } from '../../../../../../../../schemas_and_examples/meal'
 import loggedSessionRequiredSwaggerResponse from '../../../../../../../../utils/common/loggedSessionRequired.swagger-response'
 import jsonContentSwagger from '../../../../../../../../utils/jsonContent.swagger'
 import rebasePathSwagger from '../../../../../../../../utils/rebasePath.swagger'
@@ -17,7 +17,7 @@ export const root = {
 		],
 		'responses': {
 			...loggedSessionRequiredSwaggerResponse,
-			...swaggerResponse(200, 'Ok', jsonContentSwagger(NewMeal_SchemaSwagger)) // retorna as mesma informações que se faz necessario para criar uma meal
+			...swaggerResponse(200, 'Ok', jsonContentSwagger(GetMeal_SchemaSwagger)) // retorna as mesma informações que se faz necessario para criar uma meal
 		}
 
 	},
@@ -37,7 +37,7 @@ export const root = {
 		},
 		'responses': {
 			...loggedSessionRequiredSwaggerResponse,
-			...swaggerResponse(200, 'Meal updated successfully.', jsonContentSwagger(NewMeal_SchemaSwagger))
+			...swaggerResponse(200, 'Meal updated successfully.', jsonContentSwagger(GetMeal_SchemaSwagger))
 		}
 
 	},
--- src/swagger/schemas_and_examples/food/index.ts
+++ src/swagger/schemas_and_examples/food/index.ts
@@ -19,4 +19,41 @@ export const NewFood_SchemaSwagger = {
 	},
 	'required': ['quantity', 'unit']
 }
-  
+
+export const GetAllFoods_SchemaSwagger = {
+	'type': 'array',
+	'items': {
+		'type': 'object',
+		'properties': {
+			'quantity': {
+				'type': 'number',
+				'description': 'Quantity of the food item',
+			},
+			'unit': {
+				'type': 'string',
+				'description': 'Unit of measurement for the quantity (e.g., grams, liters)',
+			},
+			'obs': {
+				'type': 'string',
+				'description': 'Optional notes or observations about the food',
+			}
+		}
+	}
+}
+export const GetFoodById_SchemaSwagger = {
+	'type': 'object',
+	'properties': {
+		'quantity': {
+			'type': 'number',
+			'description': 'Quantity of the food item',
+		},
+		'unit': {
+			'type': 'string',
+			'description': 'Unit of measurement for the quantity (e.g., grams, liters)',
+		},
+		'obs': {
+			'type': 'string',
+			'description': 'Optional notes or observations about the food',
+		}
+	}
+}
